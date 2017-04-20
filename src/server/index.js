8a0b9e60347aaf4f0d681426eaa1af2ab0d80c66
Arturs Girons <arturs.girons@gmail.com>
2017-04-20 20:24:24 +0300

    Cleaning glasses

--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,53 @@
 import logo from '../../assets/logo.png'
 
+import css from '../styles/css.module.css'
+import scss from '../styles/scss.module.scss'
+import sass from '../styles/sass.module.sass'
+
 const requestListener = (req, res) => {
+  if(global.frontful.environment.coldreload.state) {
+    console.log('Restoring state', global.frontful.environment.coldreload.state)
+  }
+
   res.end(`
     <!DOCTYPE html>
     <html lang="en">
       <head>
         <title>Frontful playground</title>
+        <link rel="stylesheet" href="${global.frontful.environment.assets.css.main}">
       </head>
       <body>
         <h3>
           Frontful playground
         </h3>
+
+        <div class="css">
+          CSS styling
+        </div>
+        <div class="${css.css}">
+          CSS Module styling
+        </div>
+
+        <div class="scss">
+          SCSS styling
+        </div>
+        <div class="${scss.scss}">
+          SCSS Module styling
+        </div>
+
+        <div class="sass">
+          SASS styling
+        </div>
+        <div class="${sass.sass}">
+          SASS Module styling
+        </div>
+
         <div>
           <img src="${logo}"/>
         </div>
-        <script src="${'/assets/vendor.js'}"></script>
-        <script src="${'/assets/main.js'}"></script>
+
+        <script src="${global.frontful.environment.assets.js.vendor}"></script>
+        <script src="${global.frontful.environment.assets.js.main}"></script>
       </body>
     </html>
   `)
