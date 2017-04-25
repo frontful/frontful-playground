b6d5e82ca72a4e53c942f2b899750ab851078795
Arturs Girons <arturs.girons@gmail.com>
2017-04-25 15:27:52 +0300

    Hash this and that

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,6 @@
-import logo from '../../assets/logo.png'
-
-import css from '../styles/css.module.css'
-import scss from '../styles/scss.module.scss'
-import sass from '../styles/sass.module.sass'
+import frontfulAsset from '../../assets/frontful.png'
 
 const requestListener = (req, res) => {
-  if(global.frontful.environment.coldreload.state) {
-    console.log('Restoring state', global.frontful.environment.coldreload.state)
-  }
-
   res.end(`
     <!DOCTYPE html>
     <html lang="en">
@@ -17,35 +9,11 @@ const requestListener = (req, res) => {
         <link rel="stylesheet" href="${global.frontful.environment.assets.css.main}">
       </head>
       <body>
-        <h3>
-          Frontful playground
-        </h3>
-
-        <div class="css">
-          CSS styling
+        <div class="container">
+          <div class="content">
+            <img src="${frontfulAsset}"/>
+          </div>
         </div>
-        <div class="${css.css}">
-          CSS Module styling
-        </div>
-
-        <div class="scss">
-          SCSS styling
-        </div>
-        <div class="${scss.scss}">
-          SCSS Module styling
-        </div>
-
-        <div class="sass">
-          SASS styling
-        </div>
-        <div class="${sass.sass}">
-          SASS Module styling
-        </div>
-
-        <div>
-          <img src="${logo}"/>
-        </div>
-
         <script src="${global.frontful.environment.assets.js.vendor}"></script>
         <script src="${global.frontful.environment.assets.js.main}"></script>
       </body>
@@ -53,6 +21,4 @@ const requestListener = (req, res) => {
   `)
 }
 
-// Server index must export request listener
-// Request listener can also be an Express app
 export default requestListener
