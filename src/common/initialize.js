ad1567f1e9f3d814ddd4b27661c71ee0142adbdf
Arturs Girons <arturs.girons@gmail.com>
2017-06-07 01:00:33 +0300

    Pixel

--- src/common/initialize.js
+++ src/common/initialize.js
@@ -1,13 +1,10 @@
 import {Models} from 'frontful-model'
 import {Resolver} from 'frontful-resolver'
 
-function initialize(element, options) {
+function initialize(element, {req, res} = {}) {
   const models = new Models({
     config: {
-      'frontful-router': {
-        req: options && options.req,
-        res: options && options.res,
-      }
+      'frontful-router': {req, res}
     }
   })
 
