4db6e35b0278d0636b230587745eec3388b2c96d
Arturs Girons <arturs.girons@gmail.com>
2017-07-16 01:15:50 +0300

    Changes in environment support

--- src/server/mounts/main.js
+++ src/server/mounts/main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Views from '../../views'
 import compileContext from './utils/compileContext'
+import environment from 'frontful-environment'
 import express from 'express'
 import serverConfig from 'frontful-config/server'
 
@@ -8,22 +9,22 @@ const app = express()
 
 app.use((req, res, next) => {
   compileContext(<Views/>, {req, res}).then((context) => {
-    const assets = global.frontful.environment.assets
+    const bundle = environment.bundle
     res.send(`
       <!DOCTYPE html>
       <html lang="en">
         <head>
           <title>${serverConfig.appName}</title>
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no" />
-          <link rel="stylesheet" href="${assets.css.main}">
+          <link rel="stylesheet" href="${bundle.css.main}">
           ${context.style}
           <noscript id="sidx"></noscript>
         </head>
         <body>
           <div id="app">${context.view}</div>
           ${context.config}
-          <script src="${assets.js.vendor}"></script>
-          <script src="${assets.js.main}"></script>
+          <script src="${bundle.js.vendor}"></script>
+          <script src="${bundle.js.main}"></script>
         </body>
       </html>
     `)
